ca54567320cd90c48307ebed8fca300eeebba497
Mle4nik <abdullaev2606@mail.ru>
2024-03-30 20:34:31 +0300

    make animation

--- src/components/Faqs.jsx
+++ src/components/Faqs.jsx
@@ -1,45 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CROSS from '../assets/faqs/cross.svg'
+import {BsPlusLg} from "react-icons/bs";
+
 export default function Faqs() {
-  return (
-    <>
-    <div>
-        <div className='mx-auto tracking-wide py-24 font-[Geist] max-w-[1440px]'>
-            <div className="font-bold text-4xl text-center mb-12">
-                Частые вопросы
-            </div>
-            <div className="text-2xl text-[#000C71] space-y-6">
-                <div className="flex mx-12 space-x-8">
-                    <div className="border-2 p-8 w-1/2 rounded-xl flex items-center">
-                        <div className="w-auto mr-8 border-2 rounded-xl">
-                            <img className="m-3 w-8" src={CROSS} alt=""/>
+
+    const Question = ({title}) => {
+        return (
+            btnActive ?
+                <div onClick={() => setBtnActive(false)}
+                    className="h-auto text-white p-8 w-1/2 rounded-xl flex items-center hover:shadow-lg hover:shadow-[#835EF0] all duration-300 cursor-pointer bg-[#643CDA]">
+                    <div className="flex flex-col">
+                        <div className="flex items-center">
+                            <div
+                                className="w-auto mr-8 border-2 border-white rounded-xl bg-white text-white all duration-300">
+                                {/*<img className="m-3 w-8" src={CROSS} alt=""/>*/}
+                                <BsPlusLg className="m-3 text-4xl text-[#643CDA] all duration-300"/>
+                            </div>
+                            <h4 className="font-bold">{title}</h4>
                         </div>
-                        <h4>Сколько стоит сервис?</h4>
-                    </div>
-                    <div className="border-2 p-8 w-1/2 rounded-xl flex items-center">
-                        <div className="w-auto mr-8 border-2 rounded-xl">
-                            <img className="m-3 w-8" src={CROSS} alt=""/>
+                        <div>
+                            <p className="ml-24 w-4/5 text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                         </div>
-                        <h4>Как начать работу?</h4>
                     </div>
                 </div>
-                <div className="flex mx-12 space-x-8">
-                    <div className="border-2 p-8 w-1/2 rounded-xl flex items-center">
-                        <div className="w-auto mr-8 border-2 rounded-xl">
-                            <img className="m-3 w-8" src={CROSS} alt=""/>
+                :
+                <div onClick={() => setBtnActive(true)}
+                    className="h-auto border p-8 w-1/2 rounded-xl flex items-center hover:shadow-lg hover:shadow-[#835EF0] all duration-300 cursor-pointer">
+                    <div className="flex flex-col">
+                        <div className="flex items-center">
+                            <div
+                                className="w-auto mr-8 border-2 rounded-xl hover:bg-[#835EF0] hover:border-[#835EF0] all duration-300 hover:text-white">
+                                {/*<img className="m-3 w-8" src={CROSS} alt=""/>*/}
+                                <BsPlusLg className="m-3 text-4xl"/>
+                            </div>
+                            <h4>{title}</h4>
                         </div>
-                        <h4>Можно ли заказать разработку у вас?</h4>
                     </div>
-                    <div className="border-2 p-8 w-1/2 rounded-xl flex items-center">
-                        <div className="w-auto mr-8 border-2 rounded-xl">
-                            <img className="m-3 w-8" src={CROSS} alt=""/>
+                </div>
+        )
+    }
+
+    const [btnActive, setBtnActive] = useState(false)
+
+    return (
+        <>
+            <div>
+                <div className='mx-auto tracking-wide py-24 font-[Geist] max-w-[1440px]'>
+                    <div className="font-bold text-4xl text-center mb-12">
+                        Частые вопросы
+                    </div>
+                    <div className="text-2xl text-[#000C71] space-y-6">
+                        <div className="flex mx-12 space-x-8">
+                            <Question title="Сколько стоит сервис?"
+                                      description="lorem "/>
+                            <Question title="Как начать работу?"/>
                         </div>
-                        <h4>Могу ли я подключить коллег или клиентов?</h4>
+                        <div className="flex mx-12 space-x-8">
+                            <Question title="Можно ли заказать разработку у вас?"/>
+                            <Question title="Могу ли я подключить коллег или клиентов?"/>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-    </>
+        </>
     )
-  }
+}
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,34 +3,45 @@ import footer_logo from '../assets/footer/footer_logo.svg';
 import bot3 from '../assets/footer/bot3.svg';
 
 export default function Footer() {
-  return (
-    <>
-      <div className="bg-light-purple">
-        <div className="h-auto px-12 w-full font-normal overflow-hidden tracking-tighter max-w-[1440px] mx-auto">
-          <div className="font-normal py-16 text-8xl tracking-tighter text-white font-[Geist] ">
-            sale@moscoweb.agency
-          </div>
-          <div className="font-[Geist] tracking-tighter mb-8 font-medium text-[#B4C0FF] text-6xl">
-            Напишите нам
-          </div>
 
-          <div className="flex justify-between items-end">
-            <div className="w-1/4">
-              <img src={bot3} alt="Bot" />
+    const Link = ({link, children, className}) => {
+        return (
+            <div className="link">
+                <a className={className} href={link} target="_blank">{children}</a>
             </div>
-            <div className='flex flex-col justify-start items-end'>
-              <div className="w-1/4 mb-7">
-                <img src={footer_logo} className="right-0" alt="logo" />
-              </div>
-              <div className='flex flex-col tracking-tighter justify-center items-end text-white text-6xl gap-4 font-[Geist] font-medium mb-16'>
-                <a className='border-b-4 border-white' href="#">Youtube</a>
-                <a href="#">t.me/moscoweb</a>
-                <a href="#">inst.com/mweb.agency</a>
-              </div>
+        )
+    }
+
+    return (
+        <>
+            <div className="bg-light-purple">
+                <div
+                    className="h-auto px-12 w-full font-normal overflow-hidden tracking-tighter max-w-[1440px] mx-auto">
+                    <div className="font-normal py-16 text-8xl tracking-tighter text-white font-[Geist] ">
+                        <a href="sale@moscoweb.agency" target="_blank">sale@moscoweb.agency</a>
+                    </div>
+                    <div className="font-[Geist] tracking-tighter mb-8 font-medium text-[#B4C0FF] text-6xl">
+                        Напишите нам
+                    </div>
+
+                    <div className="flex justify-between items-end">
+                        <div className="w-1/4">
+                            <img src={bot3} alt="Bot"/>
+                        </div>
+                        <div className='flex flex-col justify-start items-end'>
+                            <div className="w-1/4 mb-7">
+                                <img src={footer_logo} className="right-0" alt="logo"/>
+                            </div>
+                            <div
+                                className='flex flex-col tracking-tighter justify-center items-end text-white text-6xl gap-4 font-[Geist] font-medium mb-16'>
+                                <Link className="border-b-4 border-white" link="https://www.youtube.com/@agatsarsky">Youtube</Link>
+                                <Link link="https://t.me/moscoweb">t.me/moscoweb</Link>
+                                <Link link="https://www.instagram.com/mweb.agency/">inst.com/mweb.agency</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
