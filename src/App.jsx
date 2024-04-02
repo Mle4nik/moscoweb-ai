07eaa37aae8e2a3cc2fd0856913e291dce1b6b63
Ten-Dark <hromaplay844@gmail.com>
2024-04-02 15:26:41 +0300

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <main>
             <Header/>
             <Categories />
-            <Services/>*/}
+            <Services/>
             <Integration /> 
             <Subscription />
             <Faqs />
